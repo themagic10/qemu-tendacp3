a948a2336b6ecf71e1e9e172f90dcdc624898412
Stefano Casolari <stefano@garp.it>
2026-09-16 02:41:52 +0200

    added non docker start script

---
+++ scripts/start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+cd "$(dirname "$0")"
+cd ../build/
+
+./qemu-system-arm -s -S -M fullhan8626v100 \
+	-cpu arm1176 \
+	-m 48M \
+	-d unimp,int,mmu,guest_errors -D errvirdump.log \
+	-icount shift=0 -rtc clock=vm \
+	-kernel ../tendaimages/uboot.dd \
+	-drive file=../tendaimages/firmware.bin,format=raw,if=mtd \
+	-serial stdio -monitor telnet:0.0.0.0:8888,server,nowait
